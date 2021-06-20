907180282a685de118a27b6f7fa67d7db57e6038
Phil Hunt <phil@deviantlogic.co.uk>
2021-06-20 17:17:35 +0100

    Testing that .gitignore ignores docker-compose.override.yml

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
+const http = require("http");
 
 const app = express();
 
 //
-// Throws an error if the PORT environment variable is missing.
+// Throws an error if the any environment variable is missing.
 //
 if (!process.env.PORT) {
     throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
+
+if (!process.env.VIDEO_STORAGE_HOST) {
+    throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
+}
+
+if (!process.env.VIDEO_STORAGE_PORT) {
+    throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
 }
 
 //
@@ -16,28 +23,29 @@ if (!process.env.PORT) {
 //
 const PORT = process.env.PORT;
 
+// Configure the connection to the video storage microservice we just built
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+
 //
-// Registers a HTTP GET route for video streaming.
+// Forwards the GET to the video route to the video storage microservce
 //
 app.get("/video", (req, res) => {
-
-    const videoPath = path.join("./videos", "SampleVideo_1280x720_1mb.mp4");
-    fs.stat(videoPath, (err, stats) => {
-        if (err) {
-            console.error("An error occurred ");
-            res.sendStatus(500);
-            return;
-        }
-
-        res.writeHead(200, {
-            "Content-Length": stats.size,
-            "Content-Type": "video/mp4",
+    const forwardRequest = http.request (
+        {
+            host: VIDEO_STORAGE_HOST,
+            port: VIDEO_STORAGE_PORT,
+            path: '/video?path=SampleVideo_1280x720_1mb.mp4',
+            method: 'GET',
+            headers: req.headers
+        },
+        // get the response to the forwarded request
+        forwardResponse => {
+            res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
+            forwardResponse.pipe(res);
         });
-        fs.createReadStream(videoPath).pipe(res);
-    });
+    req.pipe(forwardRequest);
 });
-
-//
 // Starts the HTTP server.
 //
 app.listen(PORT, () => {
